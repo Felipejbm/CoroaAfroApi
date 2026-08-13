83974ddfb31ff694b20abb1b8eff64005573ed4c
Felipejbm <felipejbmello.etec@gmail.com>
2026-08-13 08:45:50 -0300

    feat(rotas): adiciona rotas para popular o banco

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ from sqlalchemy.orm import Session
 
 
 from database import Base, engine, get_db
-from schemas import EmpreendedorCreate, LoginReq
-from models import Empreendedor
+from schemas import (
+    EmpreendedorCreate, 
+    LoginReq, 
+    TrilhaCreate, 
+    AtividadeCreate,
+    MensagemChatCreate,
+    MentorCreate, 
+    PostagemChatCreate,
+    TransacoesCreate,
+    SaldoCreate,
+    MetricasMarketingCreate
+)
+from models import (
+    Empreendedor, 
+    Trilha, 
+    Atividade, 
+    MensagemChat, 
+    Mentor, 
+    PostagemChat,
+    Transacao,
+    Saldo,
+    MetricasMarketing
+)
 
 Base.metadata.create_all(bind=engine) 
 app = FastAPI()
@@ -67,3 +88,146 @@ def criar_empreendedor(empreendedor: EmpreendedorCreate, db: Session = Depends(g
         "Msg": "Empreendedor criado com sucesso!",
         "Empreendedor": novo_empreendedor
     }
+
+@app.post('/criar-trilha')
+def criar_trilha(trilha: TrilhaCreate, db: Session = Depends(get_db)):
+    nova_trilha = Trilha(
+        titulo = trilha.titulo,
+        tipo_trilha = trilha.tipo_trilha
+    )
+
+    db.add(nova_trilha)
+    db.commit()
+    db.refresh(nova_trilha)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": nova_trilha
+    }
+
+@app.post('/criar-atividade')
+def criar_atividade(atividade: AtividadeCreate, db: Session = Depends(get_db)):
+    nova_atividade = Atividade(
+        titulo_tarefa = atividade.titulo_tarefa,
+        conteudo = atividade.conteudo
+    )
+
+    db.add(nova_atividade)
+    db.commit()
+    db.refresh(nova_atividade)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": nova_atividade
+    }
+
+@app.post('/criar-mentor')
+def criar_mentor(mentor: MentorCreate, db: Session = Depends(get_db)):
+    novo_mentor = Mentor(
+        nome = mentor.nome,
+        especialidade = mentor.especialidade,
+        biografia = mentor.biografia
+    )
+
+    db.add(novo_mentor)
+    db.commit()
+    db.refresh(novo_mentor)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": novo_mentor
+    }
+
+@app.post('/criar-mensagem')
+def criar_mensagem(mensagem: MensagemChatCreate, db: Session = Depends(get_db)):
+    nova_mensagem = MensagemChat(
+        texto_mensagem = mensagem.texto_mensagem,
+        data_envio = mensagem.data_envio,
+        lida = mensagem.lida
+    )
+
+    db.add(nova_mensagem)
+    db.commit()
+    db.refresh(nova_mensagem)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": nova_mensagem
+    }
+
+@app.post('/criar-postagem')
+def criar_postagem(postagem: PostagemChatCreate, db: Session = Depends(get_db)):
+    nova_postagem = PostagemChat(
+       conteudo_texto = postagem.conteudo_texto,
+       midia_url = postagem.midia_url,
+       data_publicacao = postagem.data_publicacao
+    )
+
+    db.add(nova_postagem)
+    db.commit()
+    db.refresh(nova_postagem)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": nova_postagem
+    }
+
+@app.post('/adicionar-transacao')
+def adicionar_trasacao(transacao: TransacoesCreate, db: Session = Depends(get_db)):
+    nova_transacao = Transacao(
+        valor = transacao.valor,
+        data = transacao.data,
+        status = transacao.status
+    )
+
+    db.add(nova_transacao)
+    db.commit()
+    db.refresh(nova_transacao)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": nova_transacao
+    }
+
+@app.post('/adicionar-saldo')
+def adicionar_saldo(saldo: SaldoCreate, db: Session = Depends(get_db)):
+    novo_saldo = Saldo(
+        saldo = saldo.saldo,
+        meta_faturamento = saldo.meta_faturamento,
+        data = saldo.data,
+        total_entradas = saldo.total_entradas,
+        total_saidas = saldo.total_saidas,
+        valor_inicial = saldo.valor_inicial,
+        valor_final =  saldo.saldo_final
+    )
+
+    db.add(novo_saldo)
+    db.commit()
+    db.refresh(novo_saldo)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": novo_saldo
+    }
+
+@app.post('/buscar-metricas-marketing')
+def buscar_metricas_marketing(metricas: MetricasMarketingCreate, db: Session = Depends(get_db)):
+    novas_metricas = MetricasMarketing(
+        data_coleta = metricas.data_coleta,
+        seguidores_total = metricas.seguidores_total,
+        alcance_postagem = metricas.alcance_postagem,
+        engajamento_taxa = metricas.engajamento_taxa,
+        cliques_bio = metricas.cliques_bio
+    )
+
+    db.add(novas_metricas)
+    db.commit()
+    db.refresh(novas_metricas)
+
+    return{
+        "Msg": "Trilha criada com sucesso!",
+        "Empreendedor": novas_metricas
+    }
+
+
+
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean
 from datetime import datetime 
 
 from database import Base
@@ -13,3 +13,71 @@ class Empreendedor(Base):
     senha = Column(String, nullable=False)
     telefone = Column(String, nullable=False)
     data_cadastro = Column(DateTime, default=datetime.now, nullable=False)
+
+class Trilha(Base):
+    __tablename__= "trilha"
+
+    id_trilha = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String, nullable=False)
+    tipo_trilha = Column(String, unique=True, nullable=False)
+
+class Atividade(Base):
+    __tablename__= "atividade"
+
+    id_atividade = Column(Integer, primary_key=True, index=True)
+    titulo_tarefa = Column(String, nullable=False)
+    conteudo = Column(String, unique=True, nullable=False)
+
+class Mentor(Base):
+    __tablename__= "mentor"
+
+    id_mentor = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    especialidade = Column(String, nullable=False)
+    biografia = Column(String, nullable=False)
+
+class MensagemChat(Base):
+    __tablename__="mensagem_chat"
+
+    id_mensagem = Column(Integer, primary_key=True, index=True)
+    texto_mensagem = Column(String, nullable=False)
+    data_envio = Column(DateTime, default=datetime.now, nullable=False)
+    lida = Column(String, nullable=False)
+
+class PostagemChat:
+    __tablename__ = "postagem"
+
+    id_post = Column(Integer, primary_key=True, index=True)
+    conteudo_texto = Column(String, nullable=False)
+    midia_url = Column(String)
+    data_publicacao = Column(DateTime, default=datetime.now, nullable=False)
+
+class Transacao:
+    __tablename__ = "transacoes"
+
+    id_transacao = Column(Integer, primary_key=True, index=True)
+    valor = Column(Float, nullable=False)
+    data = Column(DateTime, nullable=False)
+    status = Column(Boolean, nullable=False)
+
+class Saldo:
+    __tablename__ = "saldo"
+
+    id_saldo = Column(Integer, primary_key=True, index=True)
+    saldo = Column(Float, nullable=False)
+    meta_faturamento = Column(Float, nullable=False)
+    data = Column(DateTime, nullable=False)
+    total_entradas = Column(Integer, nullable=False)
+    total_saidas = Column(Integer, nullable=False)
+    valor_inicial = Column(Float, nullable=False)
+    saldo_final = Column(Float, nullable=False)
+
+class MetricasMarketing:
+    __tablename__ = "metricas_marketing"
+
+    id_saldo = Column(Integer, primary_key=True, index=True)
+    data_coleta = Column(DateTime,default=datetime.now, nullable=False)
+    seguidores_total = Column(Integer, nullable=False)
+    alcance_postagem = Column(Integer, nullable=False)
+    engajamento_taxa = Column(Integer, nullable=False)
+    cliques_bio = Column(Integer, nullable=False)
