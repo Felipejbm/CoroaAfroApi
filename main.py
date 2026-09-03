470621bb5764df23f831192e8f2542fe8b346ff9
FelipeJbm <felipejbmello.etec@gmail.com>
2026-09-03 20:47:33 -0300

    feat(db): adiciona criação do banco

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from database import Base, engine, get_db
-from schemas import (
-    EmpreendedorCreate, 
-    LoginReq, 
-    TrilhaCreate, 
-    AtividadeCreate,
-    MensagemChatCreate,
-    MentorCreate, 
-    PostagemChatCreate,
-    TransacoesCreate,
-    SaldoCreate,
-    MetricasMarketingCreate,
-    EmpresaCreate
-)
-from models import (
-    Empreendedor, 
-    Trilha, 
-    Atividade, 
-    MensagemChat, 
-    Mentor, 
-    PostagemChat,
-    Transacao,
-    Saldo,
-    MetricasMarketing
-)
+from database import Base, engine
+
 from routers import all_router
 
 Base.metadata.create_all(bind=engine) 
